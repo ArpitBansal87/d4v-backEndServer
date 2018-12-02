94e125e7c775786cb2933e28b219596e275268d3
Arpit Bansal <arpit.bansal@outlook.com>
2018-12-02 07:42:05 +0530

    Changes for User manipulation

--- common/models/credential.js
+++ common/models/credential.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function(Credential) {
+    Credential.observe('after save', function setRoleMapping(ctx, next) {
+        if (ctx.instance) {
+          if(ctx.isNewInstance) {
+      
+            // look up role based on type
+            //
+            // Role.find({where: {name: ctx.instance.type}}, function(err, role) {
+            //   if (err) {return console.log(err);}
+      
+            //   RoleMapping.create({
+            //     principalType: "USER",
+            //     principalId: ctx.instance.id,
+            //     roleId: role.id
+            //   }, function(err, roleMapping) {
+      
+            //     if (err) {return console.log(err);}
+      
+            //     console.log('User assigned RoleID ' + role.id + ' (' + ctx.instance.type + ')');
+      
+            //   }):
+      
+            // });
 
+            console.log("value for  ctx.instance.type: "+ctx.instance.type)
+      
+          }
+        }
+        next();
+      });
 };
